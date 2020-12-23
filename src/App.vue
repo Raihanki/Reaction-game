8749668ac76a092a4e5b6b1480c6a399f8ed6a84
Raihanki <Raihanhori>
2020-12-23 13:50:47 +0700

    Make a reaction click game

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Gaou Reaction Game</h1>
+  <button @click="startGame" class="playButton" :disabled="isPlaying">play</button>
+  <Block :delay="delay" v-if="isPlaying" @end="endGame"></Block>
+  <Result :timeResult="score" v-if="gameOver"></Result>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue';
+import Result from './components/Result.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      gameOver: false,
+    }
+  },
+  methods: {
+    startGame() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000;
+      this.gameOver = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.gameOver = true;
+      this.isPlaying = false;
+    }
+  },
 }
 </script>
 
@@ -20,7 +41,26 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #0d1c00;
   margin-top: 60px;
+}
+.playButton {
+  padding: 10px 20px;
+  color: white;
+  border: none;
+  background: #5bb75b;
+  border-radius: 5px;
+  font-size: large;
+}
+.playButton:hover {
+  background: green;
+}
+.playButton:focus {
+  outline-width: thin;
+  outline-color: darkgreen;
+}
+.playButton[disabled] {
+  opacity: 0.4;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="result">
+    <p>Reaction Time : {{ timeResult }}</p>
+    <span>Thanks for playing you have a {{ reflex }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['timeResult'],
+  data() {
+    return {
+      reflex: this.checkReflex(),
+    }
+  },
+  methods: {
+    checkReflex() {
+      if ( this.timeResult <= 250 ) {
+        return 'NINJA REFLEX :o';
+      } else if ( this.timeResult < 350 ) {
+        return 'RAPID REFLEX :u';
+      } else if ( this.timeResult < 500 ) {
+        return 'CAT REFLEX :)';
+      } else if ( this.timeResult < 600) {
+        return 'TURTLE POWER REFLEX :|';
+      } else {
+        return 'SNAIL REFLEX :('
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .result {
+    color: darkgreen;
+    font-size: x-large;
+  }
+</style>
